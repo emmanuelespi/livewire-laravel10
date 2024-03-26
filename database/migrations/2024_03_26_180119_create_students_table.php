f608991378927cf3937d9004a93d2a6821269491
Emmanuel Espinoza <eespinest87@gmail.com>
2024-03-26 12:06:26 -0600

    Creación proyecto, instalación de livewire y creación de migraciones y modelos

---
+++ database/migrations/2024_03_26_180119_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('class_id')->constrained('classes'); //Campo que define relación entre Classes y Students
+            $table->foreignId('section_id')->constrained('sections'); //Campo que define relación entre Section y Students
+            $table->string('name');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
